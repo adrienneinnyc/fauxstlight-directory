49f942c996fa903a1a944488d55039d0a588d73d
Adrienne Johnson <adriennetjohnson@gmail.com>
2020-07-02 19:01:34 -0400

    Script for heroku build

--- server/db/db.js
+++ server/db/db.js
@@ -2,6 +2,8 @@ const Sequelize = require("sequelize");
 
 const databaseName = "fauxstlight";
 
+console.log("URL*****", process.env.DATABASE_URL);
+
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
